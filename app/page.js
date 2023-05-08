0943fbff697e02b8f890ebe5c4e1a592e859437f
vannelo <vannelo@vannelos-Laptop.local>
2023-05-07 18:22:50 -0600

    Add animations

--- app/page.js
+++ app/page.js
@@ -1,113 +1,197 @@
-import Image from 'next/image'
+"use client";
+import Image from "next/image";
+import styles from "./home.module.sass";
+import Link from "next/link";
+import { motion } from "framer-motion";
 
 export default function Home() {
+  const goToSection = (id) => {
+    document.getElementById(id).scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <nav className={styles.navDesktop}>
+        <div className={styles.link} onClick={() => goToSection("home")}>
+          HOME
         </div>
+        <div className={styles.link} onClick={() => goToSection("historia")}>
+          HISTORIA
+        </div>
+        <div className={styles.link} onClick={() => goToSection("prensa")}>
+          PRENSA
+        </div>
+        <div className={styles.link} onClick={() => goToSection("contacto")}>
+          CONTACTO
+        </div>
+      </nav>
+      <div className={styles.header}>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 2 }}
+        >
+          <div className={styles.img}>
+            <Image
+              src="/img/jorge-sanchez.png"
+              width={1532}
+              height={712}
+              alt="Jorge Sanchez"
+              className={styles.image}
+            />
+          </div>
+        </motion.div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+      <main className={styles.pageHome} id="home">
+        <motion.div
+          initial={{ y: 100, opacity: 0.5 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 2 }}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+          <section className={styles.mainPhoto}>
+            <Image
+              src="/img/photo-1.jpg"
+              width={1280}
+              height={1600}
+              alt="Jorge Sanchez"
+            />
+          </section>
+        </motion.div>
+        <section className={styles.historia} id="historia">
+          <motion.div
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 1.5 }}
+            viewport={{ once: true }}
+          >
+            <h2>HISTORIA</h2>
+            <p>
+              Do laborum esse est ipsum velit sunt proident pariatur culpa
+              exercitation excepteur dolor irure. Duis exercitation amet tempor
+              exercitation anim ullamco nisi adipisicing.
+            </p>
+            <p>
+              Deserunt ipsum nulla aute dolor et nulla aute sit labore occaecat
+              esse. Tempor elit in culpa adipisicing reprehenderit dolore aliqua
+              excepteur dolor ex tempor fugiat amet. Officia do quis ipsum est
+              et tempor.
+            </p>
+            <p>
+              Incididunt minim qui non nulla reprehenderit Lorem sint culpa
+              aute. Culpa amet veniam pariatur cillum nisi laborum eu consequat.
+            </p>
+          </motion.div>
+        </section>
+        <section className={styles.prensa} id="prensa">
+          <motion.div
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 1.5 }}
+            viewport={{ once: true }}
+          >
+            <h2>PRENSA</h2>
+            <p>
+              Deserunt magna Lorem minim sunt amet aliqua consectetur
+              consectetur culpa sit eiusmod.
+            </p>
+          </motion.div>
+          <div className={styles.gallery}>
+            <motion.div
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              transition={{ duration: 1.5 }}
+              viewport={{ once: true }}
+            >
+              <div className={styles.principal}>
+                <Image
+                  src="/img/gallery-1.jpg"
+                  width={2048}
+                  height={1365}
+                  alt="Jorge Sanchez - Galeria"
+                />
+              </div>
+            </motion.div>
+            <div className={styles.divider} />
+            <div className={styles.photos}>
+              <div className={styles.photo}>
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  whileInView={{ opacity: 1 }}
+                  transition={{ duration: 1.5 }}
+                  viewport={{ once: true }}
+                >
+                  <Image
+                    src="/img/gallery-2.jpg"
+                    width={1066}
+                    height={1600}
+                    alt="Jorge Sanchez - Galeria"
+                  />
+                </motion.div>
+              </div>
+              <div className={styles.photo}>
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  whileInView={{ opacity: 1 }}
+                  transition={{ duration: 1.5, delay: 0.3 }}
+                  viewport={{ once: true }}
+                >
+                  <Image
+                    src="/img/gallery-3.jpg"
+                    width={1066}
+                    height={1600}
+                    alt="Jorge Sanchez - Galeria"
+                  />
+                </motion.div>
+              </div>
+              <div className={styles.photo}>
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  whileInView={{ opacity: 1 }}
+                  transition={{ duration: 1.5, delay: 0.6 }}
+                  viewport={{ once: true }}
+                >
+                  <Image
+                    src="/img/gallery-4.jpg"
+                    width={1066}
+                    height={1600}
+                    alt="Jorge Sanchez"
+                  />
+                </motion.div>
+              </div>
+              <div className={styles.photo}>
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  whileInView={{ opacity: 1 }}
+                  transition={{ duration: 1.5, delay: 0.9 }}
+                  viewport={{ once: true }}
+                >
+                  <Image
+                    src="/img/gallery-5.jpg"
+                    width={1066}
+                    height={1600}
+                    alt="Jorge Sanchez"
+                  />
+                </motion.div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className={styles.contacto} id="contacto">
+          <motion.div
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 1.5 }}
+            viewport={{ once: true }}
+          >
+            <h2>CONTACTO</h2>
+            <p>Cupidatat sint nisi minim occaecat.</p>
+            <Link href="mailto:christiancastellanosmx@gmail.com">
+              <button>jorge.mgmt@boldtalent.mx</button>
+            </Link>
+          </motion.div>
+        </section>
+        <footer>Jorge Sanchez 2013 @ Todos los derechos reservados.</footer>
+      </main>
+    </>
+  );
 }
